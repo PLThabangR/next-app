b734dd391e2d5d1cda610f85dc9d053a6d439834
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2023-10-26 00:58:14 +0200

    fetching user using server component

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const NewUserPage = () => {
-  return (
-    <div>NewUser</div>
+
+
+const NewUserPage =async () => {
+ 
+    return (
+    <>
+       
+
+    </>
   )
 }
 
