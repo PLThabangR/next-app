d7a61d81ed21a5c0ea52706ba47e3f042cb0c2bd
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2023-10-26 00:29:54 +0200

    Used server components to manipulate the dom using use client

---
+++ app/components/productCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AddToCard from './AddToCard'
+
+const productCard = () => {
+  return (
+    <div>
+       
+    <AddToCard/>
+
+    </div>
+  )
+}
+
+export default productCard
